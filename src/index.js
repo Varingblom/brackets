55b62158983e4bc061fd232bb2b6bdafbab932c4
Varingblom <106687251+Varingblom@users.noreply.github.com>
2022-08-12 23:48:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str
+  for(let i = 0; i < str.length; i++) {
+  for(let x = 0; x < bracketsConfig.length; x++) {
+    if (arr.includes(bracketsConfig[x].join(""))) {
+      arr = arr.slice(0, arr.indexOf(bracketsConfig[x].join(""))) + arr.slice(arr.indexOf(bracketsConfig[x].join("")) + 2)
+      }
+      }
+  }
+  return arr.length === 0 ? 1 : 0
 }
